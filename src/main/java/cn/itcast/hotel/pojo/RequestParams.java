e159455852385c164aef6572bf348d041587a313
Maplefse <3308945568@qq.com>
2022-05-19 10:54:53 +0800

    删除target

---
+++ src/main/java/cn/itcast/hotel/pojo/RequestParams.java
@@ -0,0 +1,6 @@
+package cn.itcast.hotel.pojo;/**
+ * Created with IntelliJ IDEA.
+ * @Author: fxiao
+ * @Version: 2022/05/12/23:30
+ */public class RequestParams {
+}
---
+++ src/main/java/cn/itcast/hotel/web/HotelController.java
@@ -0,0 +1,6 @@
+package cn.itcast.hotel.web;/**
+ * Created with IntelliJ IDEA.
+ * @Author: fxiao
+ * @Version: 2022/05/12/23:37
+ */public class HotelController {
+}
